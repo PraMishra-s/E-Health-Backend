72382d07098b6085ef8f89288ca24b941b4cee8e
Pralad02210217 <praladmishra3@gmail.com>
2025-05-03 10:01:01 +0600

    update sameSite

--- src/common/utils/cookies.ts
+++ src/common/utils/cookies.ts
@@ -11,8 +11,8 @@ type CookiesPayloadType = {
 const defaults: CookieOptions = {
     httpOnly: true,
     secure: config.NODE_ENV === "production"? true: false,
-    sameSite: config.NODE_ENV === "production"? "none": "lax",
-    domain: "https://e-health-backend.onrender.com",
+    sameSite: config.NODE_ENV === "production"? "lax": "lax",
+    domain: "e-health-backend.onrender.com",
     partitioned: true,
 }
 export const REFRESH_PATH = `${config.BASE_PATH}/auth/refresh`
