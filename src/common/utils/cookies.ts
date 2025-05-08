7b32a3f66643fe88859298a54eeaaf96b9bee881
Pralad02210217 <praladmishra3@gmail.com>
2025-05-08 22:07:27 +0600

    with partitioned

--- src/common/utils/cookies.ts
+++ src/common/utils/cookies.ts
@@ -12,6 +12,7 @@ const defaults: CookieOptions = {
     httpOnly: true,
     secure: config.NODE_ENV === "production"? true: false,
     sameSite: config.NODE_ENV === "production"? "none": "lax",
+    partitioned: true,
 }
 export const REFRESH_PATH = `${config.BASE_PATH}/auth/refresh`
 
