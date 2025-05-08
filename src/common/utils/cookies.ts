979eaa418a911434a39a9345fdda795314530a83
Pralad02210217 <praladmishra3@gmail.com>
2025-05-08 21:51:48 +0600

    updated sameSite

--- src/common/utils/cookies.ts
+++ src/common/utils/cookies.ts
@@ -11,7 +11,7 @@ type CookiesPayloadType = {
 const defaults: CookieOptions = {
     httpOnly: true,
     secure: config.NODE_ENV === "production"? true: false,
-    sameSite: config.NODE_ENV === "production"? "lax": "lax",
+    sameSite: config.NODE_ENV === "production"? "none": "lax",
 }
 export const REFRESH_PATH = `${config.BASE_PATH}/auth/refresh`
 
